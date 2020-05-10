d465f55e5fc0e8c7e87664df301f2ad0c6b1821b
Sagnik Modak <sagnikmodak1118@gmail.com>
2020-05-11 03:35:25 +0530

    update

--- controller/module/CrowdTest.js
+++ controller/module/CrowdTest.js
@@ -1,7 +1,7 @@
 import { ROOM_UPDATED } from '../../constants.js';
 import Distributor from '../../lib/Distributor'
 
-const { EVENT_ADDED, EVENT_JOIN, EVENT_EXIT, EVENT_SUBMIT_QUESTION, EVENT_QUESTION_BATCH, EVENT_SUBMIT_ANSWER, EVENT_EDIT_QUESTION, EVENT_DISPATCH_QUESTIONS, STUDENT_JOINED, STUDENT_EXITED } = require('../../constants.js');
+const { EVENT_ADDED, EVENT_JOIN, EVENT_EXIT, EVENT_END_TEST, EVENT_SUBMIT_QUESTION, EVENT_QUESTION_BATCH, EVENT_TEST_RESULT, EVENT_SUBMIT_ANSWER, EVENT_EDIT_QUESTION, EVENT_DISPATCH_QUESTIONS, STUDENT_JOINED, STUDENT_EXITED } = require('../../constants.js');
 const { Student, Room, Task, StudentEvent, FacultyEvent, Module } = require('../../model');
 
 export default async function ({ task_id, _id, event, pubsub }) {
@@ -59,7 +59,22 @@ export default async function ({ task_id, _id, event, pubsub }) {
       await question.save();
       return true;
     } else if(event.Descriptor === EVENT_SUBMIT_ANSWER) {
-      
+      let question = await Module.CrowdTest.CrowdQuestion.findOne({
+        _id: event.Data._id
+      });
+      let response = new Module.CrowdTest.CrowdResponse({
+        Author: {
+          _id: event.Student._id
+        },
+        Question: {
+          _id: question._id
+        },
+        Selection: event.Data.Selection,
+        Score: (event.Data.Selection === question.Correct) ? 1 : 0
+      });
+      await Module.CrowdTest.CrowdTest.updateOne({ _id: _id }, { $addToSet: { Responses: { _id: response._id } } });
+      await response.save();
+      return true;
     }
   } else {
     if(event.Descriptor === EVENT_EDIT_QUESTION) {
@@ -76,7 +91,7 @@ export default async function ({ task_id, _id, event, pubsub }) {
       }
       let distributions = await Distributor(students, questions);
       for(let batch of distributions) {
-        let e = {
+        let e = new FacultyEvent({
           Descriptor: EVENT_QUESTION_BATCH,
           Data: {
             _id: batch.student._id,
@@ -85,12 +100,35 @@ export default async function ({ task_id, _id, event, pubsub }) {
           Faculty: {
             _id: event.Faculty._id
           }
-        };
+        });
         pubsub.publish(EVENT_ADDED, { eventSub: e });
       }
       return true;
     } else if(event.Descriptor === EVENT_END_TEST) {
-
+      let test = await Module.CrowdTest.CrowdTest.findOne({ _id });
+      let responses = [];
+      for(let response of test.Responses) {
+        responses.push(await Module.CrowdTest.CrowdResponse.findOne({ _id: response._id }));
+      }
+      for(let student of test.Students) {
+        let filteredResponses = responses.filter(v => {
+          console.log(v.Author._id, student._id);
+          return v.Author._id.toString() === student._id.toString();
+        });
+        let score = filteredResponses.map(v => v.Score).reduce((a,v) => a + v);
+        let e = new FacultyEvent({
+          Descriptor: EVENT_TEST_RESULT,
+          Data: {
+            _id: student._id,
+            Score: score
+          },
+          Faculty: {
+            _id: event.Faculty._id
+          }
+        });
+        pubsub.publish(EVENT_ADDED, { eventSub: e });
+      }
+      return true;
     }
   }
   return true;
